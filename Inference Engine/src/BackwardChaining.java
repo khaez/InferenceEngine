063213ab2b81307ecc763a0b9ee21d73d745f69d
kaishley <kklingachetti@msn.com>
2014-05-24 19:18:18 +1000

    - Implement Backward Chaining

--- Inference Engine/src/BackwardChaining.java
+++ Inference Engine/src/BackwardChaining.java
@@ -1,10 +1,11 @@
+import java.util.ArrayList;
+
 /**
  * BackwardChaining.java
  * Created on 22/5/2014
  * Author: Kaishley Lingachetti (4303350)
  */
 
-import java.util.*;
  
 // to run simply do a: new BC(ask,tell) and then bc.execute()
 // ask is a propositional symbol
@@ -12,138 +13,94 @@ import java.util.*;
 // ask : p
 // tell : p=>q;q=>r;r;
  
-class BackwardChaining{
+public class BackwardChaining{
 // create variables
-public static String tell;
-public static String ask;
-public static ArrayList<String> agenda;
-public static ArrayList<String> facts;
-public static ArrayList<String> clauses;
-public static ArrayList<String> entailed;
- 
-public BackwardChaining(String a, String t){
-	// initialize variables
-	agenda  = new ArrayList<String>();
-	clauses  = new ArrayList<String>();
-	entailed  = new ArrayList<String>();
-	facts  = new ArrayList<String>();
-	tell = t;
-	ask = a;
-	init(tell);
-}
- 
- 
-// method which sets up initial values for backward chaining
-public static void init(String tell){
-	agenda.add(ask);
-	// split kb into sentences
-   String[] sentences = tell.split(";");
-	for (int i=0;i<sentences.length;i++){
-		if (!sentences[i].contains("=>")) 
-			// add the facts
-			facts.add(sentences[i]);
-		else{
-			// add the premises	
-			clauses.add(sentences[i]);
-			}
+	private static String ask;
+	private static ArrayList<String> rules;
+	private static ArrayList<String> facts;
+	private static ArrayList<String> workingList;
+	private static ArrayList<String> queued;
+	
+	public BackwardChaining(String tell, String ask)
+	{
+		TellSplitter ts = new TellSplitter(tell);
+		this.ask = ask;
+		rules = ts.getRules();
+		facts = ts.getNonRules();
+		queued = new ArrayList<String>();
+		
+		workingList.add(ask);
 	}
-}
- 
-// method which calls the main bcentails() method and returns output back to iengine
-public String execute(){
-	String output = "";
- 
-	if (bcentails()){
-		// the method returned true so it entails
-		output = "YES: ";
-		// loop through all entailed symbols in reverse
-		for (int i=entailed.size()-1;i>=0;i--){
-				if (i==0)
-					output += entailed.get(i);
-				else	
-					// no comma at the end
-					output += entailed.get(i)+", ";
- 
-			}
-	}
-	// no 
-	else{
-			output = "NO";
-	}
-	return output;		
-}
- 
-// method which runs the bc algorithm
-public boolean bcentails(){
-	// while the list of symbols are not empty
-	while(!agenda.isEmpty()){
-		// get current symbol
-		String q = agenda.remove(agenda.size()-1);
-		// add the entailed array
-		entailed.add(q);
- 
-		// if this element is a fact then we dont need to go further
-		if (!facts.contains(q)){
-			// .. but it isnt so..
-			// create array to hold new symbols to be processed 
-			ArrayList<String> p = new ArrayList<String>();
-			for(int i=0;i<clauses.size();i++){
-			// for each clause..
-					if (conclusionContains(clauses.get(i),q)){
-					// that contains the symbol as its conclusion
- 
-							ArrayList<String> temp = getPremises(clauses.get(i));
-							for(int j=0;j<temp.size();j++){
-								// add the symbols to a temp array
-								p.add(temp.get(j));
-							}
-						}						
-			}
-			// no symbols were generated and since it isnt a fact either 
-			// then this sybmol and eventually ASK  cannot be implied by TELL
-			if (p.size()==0){
-				return false;
-			}
-			else{
-					// there are symbols so check for previously processed ones and add to agenda
-					for(int i=0;i<p.size();i++){
-							if (!entailed.contains(p.get(i)))
-									agenda.add(p.get(i));
-							}
- 
- 
+	
+	public String printResult()
+	{
+		String result = "";
+		
+		if(bcPresent())
+		{
+			result = "YES: ";
+			for (int i = queued.size()-1; i >= 0; i--)
+			{
+				if(i == 0)
+					result += queued.get(i);
+				else
+					result += queued.get(i) + ", ";
 			}
 		}
- 
-	}//while end
-	return true;
-}
- 
-// methid that returns the conjuncts contained in a clause
-public static ArrayList<String> getPremises(String clause){
-	// get the premise
-	String premise = clause.split("=>")[0];
-	ArrayList<String> temp = new ArrayList<String>();
-	String[] conjuncts = premise.split("&");
-	// for each conjunct
-	for(int i=0;i<conjuncts.length;i++){
-			if (!agenda.contains(conjuncts[i]))
-					temp.add(conjuncts[i]);
+		else
+			result = "NO";
+		
+		return result;
 	}
-	return temp;
-}
- 
- 
-// method which checks if c appears in the conclusion of a given clause	
-// input : clause, c
-// output : true if c is in the conclusion of clause
-public static boolean conclusionContains(String clause, String c){
-	String conclusion = clause.split("=>")[1];
-	if (conclusion.equals(c))
+	
+	private boolean bcPresent() 
+	{
+		while(!workingList.isEmpty())
+		{
+			String workingValue = workingList.remove(workingList.size()-1);
+			
+			queued.add(workingValue);
+			
+			if(!facts.contains(workingValue))
+			{
+				ArrayList<String> premises = new ArrayList<String>();
+				
+				for(String rule : rules)
+				{
+					if(ruleEqualsTo(rule, workingValue))
+					{
+						String premise = rule.split("=>")[0];
+						String[] variables = premise.split("&");
+						
+						for(String variable : variables)
+						{
+							if(!workingList.contains(variable))
+								premises.add(variable);
+						}
+					}
+				}
+				
+				if(premises.size() == 0)
+					return false;
+				else
+				{
+					for(String premise : premises)
+					{
+						if(!queued.contains(premise))
+							workingList.add(premise);
+					}
+				}
+			}
+		}
 		return true;
-	else
-		return false;
- 
-}
- 
+	}
+	
+	private boolean ruleEqualsTo(String rule, String toCheck)
+	{
+		String splittedRule = rule.split("=>")[1];
+		if(splittedRule.equals(toCheck))
+			return true;
+		else
+			return false;
+	}
 }
