3ede7cff3ca17d90bc8dbdcdaa50ffbcf63ef84e
Azim <iikhaezii@gmail.com>
2014-05-24 17:58:50 +1000

    - Added check arguments - File inputs - Calling of the methods - Identifying which method to call

--- Inference Engine/src/InferenceEngine.java
+++ Inference Engine/src/InferenceEngine.java
@@ -1,15 +1,102 @@
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class InferenceEngine {
 
-	public static void main(String args[])
+	public static void main(String args[]) throws IOException
 	{
-		System.out.println("Hello World");
+		String _tell = "";
+		String _ask = "";
+		ArrayList<String> inputStrings = new ArrayList();
 		
-		ForwardChaining fc = new ForwardChaining("d","p2=> p3; p3 => p1; c => e; b&e => f; f&g => h; p1=>d; p1&p3 => c; a; b; p2;");
+		if(checkInput(args))
+		{
+			try{
+				FileInputStream fr = new FileInputStream(args[1]);
+				Scanner sc = new Scanner(fr);
+				while(sc.hasNextLine())
+				{
+					inputStrings.add(sc.nextLine());
+				}
+				
+				for(int i = 0; i < inputStrings.size(); i++)
+				{
+					//System.out.println(inputStrings.get(i));
+					if(inputStrings.get(i).toUpperCase().equals("TELL"))
+					{
+						_tell = inputStrings.get(i+1);
+					}
+					if(inputStrings.get(i).toUpperCase().equals("ASK"))
+					{
+						_ask = inputStrings.get(i+1);
+					}
+				}
+				
+				runEngine(args, _tell, _ask);
+			}catch (FileNotFoundException e){ 
+			    System.err.println("FileNotFoundException: " + e.getMessage());
+			}catch (IOException e){
+				System.err.println("IOException: " + e.getMessage());
+			}
+			
+		}
+		else
+			inputMessage();
+		
+/*		ForwardChaining fc = new ForwardChaining("d","p2=> p3; p3 => p1; c => e; b&e => f; f&g => h; p1=>d; p1&p3 => c; a; b; p2;");
 		System.out.println(fc.execute());
 		
 		BackwardChaining bc = new BackwardChaining("d","p2=> p3; p3 => p1; c => e; b&e => f; f&g => h; p1=>d; p1&p3 => c; a; b; p2;");
-		System.out.println(bc.execute());
+		System.out.println(bc.execute());*/
+		
+	}
+	
+	// This method will run the engine base on what method is selected
+	public static void runEngine(String file[], String tell, String ask)
+	{
+		// This if statement calls the Truth Table method
+		if(file[0].toUpperCase() == "TT")
+		{
+			// The Truth Table method goes here
+		}
+		// This if statement calls the Forward Chaining method
+		else if(file[0].toUpperCase().equals("FC"))
+		{
+			ForwardChaining fc = new ForwardChaining(ask, tell);
+			System.out.println(fc.execute());
+		}
+		// This if statement calls the Backward Chaining method
+		else if(file[0].toUpperCase() == "BC")
+		{
+			BackwardChaining bc = new BackwardChaining(ask, tell);
+			System.out.println(bc.execute());
+		}
+		
+	}
+	
+	// CheckInput method will check if the number of arguments are correct
+	public static boolean checkInput(String file[])
+	{
+		if(file.length != 2)
+		{
+			return false;
+		}
+		
+		else return true;
+	}
+	
+	// The inputMessage method will display a message if wrong arguments are inserted
+	public static void inputMessage()
+	{
+		System.out.println("\nPlease input the following format:");
+		System.out.println("Program Method File.txt");
+		System.out.println("1) TT - Truth Table");
+		System.out.println("2) FC - Forward Chaining");
+		System.out.println("3) BC - Backward Chaining");
 	}
 	
 	
