64e3a41c067e22a6a05e907c821f7f88cb94fbd5
Kaishley <kklingachetti@msn.com>
2014-05-30 03:17:00 +1000

    - Added comments

--- Inference Engine/src/BackwardChaining.java
+++ Inference Engine/src/BackwardChaining.java
@@ -10,75 +10,75 @@ public class BackwardChaining{
 // create variables
 	private ArrayList<String> rules;
 	private ArrayList<String> facts;
-	private ArrayList<String> workingList;
-	private ArrayList<String> queued;
+	private ArrayList<String> workingList;		//agenda
+	private ArrayList<String> queued;			//entailed
 	
 	public BackwardChaining(String ask, String tell)
 	{
 		TellSplitter ts = new TellSplitter(tell);
-		rules = ts.getRules();
-		facts = ts.getFacts();
+		rules = ts.getRules();						//get rules
+		facts = ts.getFacts();						//get facts
 		queued = new ArrayList<String>();
 		workingList = new ArrayList<String>();
-		workingList.add(ask);
+		workingList.add(ask);						//add ask symbol to working list(agenda)
 	}
 	
 	public String printResult()
 	{
 		String result = "";
 		
-		if(bcPresent())
+		if(bcPresent())					//if entailed
 		{
 			result = "YES: ";
-			for (int i = queued.size()-1; i >= 0; i--)
+			for (int i = queued.size()-1; i >= 0; i--)		//display in reverse in queued	
 			{
 				if(i == 0)
-					result += queued.get(i);
+					result += queued.get(i);			//if last symbol, no comma
 				else
-					result += queued.get(i) + ", ";
+					result += queued.get(i) + ", ";		//if not last symbol, comma
 			}
 		}
 		else
-			result = "NO";
+			result = "NO";			//display No if not entailed
 		
 		return result;
 	}
 	
 	private boolean bcPresent() 
 	{
-		while(!workingList.isEmpty())
+		while(!workingList.isEmpty())		//while working list of symbols not empty
 		{
-			String workingValue = workingList.remove(workingList.size()-1);
+			String workingValue = workingList.remove(workingList.size()-1);		//get symbol to work
 			
-			queued.add(workingValue);
+			queued.add(workingValue);			//add symbol to queued(entailed)
 			
-			if(!facts.contains(workingValue))
+			if(!facts.contains(workingValue))	//if symbol is a fact, no need to continue
 			{
 				ArrayList<String> premises = new ArrayList<String>();
 				
-				for(String rule : rules)
+				for(String rule : rules)		//for each rule
 				{
-					if(ruleEqualsTo(rule, workingValue))
+					if(ruleEqualsTo(rule, workingValue)) //if working symbol is at right side of rule
 					{
-						String premise = rule.split("=>")[0];
-						String[] variables = premise.split("&");
+						String premise = rule.split("=>")[0];		//get left side of rule
+						String[] variables = premise.split("&");	//split left side of rule
 						
-						for(String variable : variables)
+						for(String variable : variables)		//for each symbol on left side of rule
 						{
-							if(!workingList.contains(variable))
+							if(!workingList.contains(variable))		//add symbol to premises if not in workingList(agenda
 								premises.add(variable);
 						}
 					}
 				}
 				
 				if(premises.size() == 0)
-					return false;
+					return false;			//return false if no premises
 				else
 				{
 					for(String premise : premises)
 					{
-						if(!queued.contains(premise))
-							workingList.add(premise);
+						if(!queued.contains(premise))	//if premise not queued(entailed)
+							workingList.add(premise);	//add premise to workingList(agenda)
 					}
 				}
 			}
@@ -88,10 +88,10 @@ public class BackwardChaining{
 	
 	private boolean ruleEqualsTo(String rule, String toCheck)
 	{
-		String splittedRule = rule.split("=>")[1];
-		if(splittedRule.equals(toCheck))
-			return true;
+		String splittedRule = rule.split("=>")[1];		//get right side of rule
+		if(splittedRule.equals(toCheck))				//check if symbol is on right side of rule
+			return true;								//true if yes
 		else
-			return false;
+			return false;								//false if no
 	}
 }
